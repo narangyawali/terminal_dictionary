ce4d64d3842f93830171cedce2858898917a44de
naran gyawali <nryng123@gmail.com>
2021-08-11 14:03:45 +0545

    with history feature

--- def.py
+++ def.py
@@ -1,17 +1,25 @@
 import requests
 import json
 import sys
-
+from pathlib import Path
+#home = str(Path.home())
+home = Path.home()
 api_url = "https://api.dictionaryapi.dev/api/v2/entries/en_US/"
 
-def show_result(list):
+def show_result(list,arg):
     for i in range(len(list)):
+        defination= list[i]["definition"]
+        example= list[i]["example"]
         #print(f"({i}):{list[i]["definition"]}")
+        print(home)
         print("-------------------------------------------------------------------")
         print("(defintion):")
-        print(list[i]["definition"])
+        print(defination)
         print(f"(example):")
-        print(list[i]["example"])
+        print(example)
+        with open (f"{home}/ter_dict/history.csv","a") as history:
+            history.write(f"{arg},\"{defination}\",\"{example}\"\n")
+            
 		
 
 def dict(arg):
@@ -22,12 +30,13 @@ def dict(arg):
         result = json.loads(response.text)
         try:
             meanings = result[0]["meanings"][0]["definitions"]# [0]["definition"]
-            show_result(meanings)
+            show_result(meanings,arg)
         except:
             error_message = "meaning of {} not found due to ...".format(arg)
             print(error_message)
         # finally:
         #     return meaning
+
     else:
         return "There seems to be problem in your internet connection or ....."        
 
--- setup.sh
+++ setup.sh
@@ -2,3 +2,15 @@
 
 chmod +x ./dist/def
 sudo cp ./dist/def /bin/
+
+
+#creating directory and file
+
+if [ -d ~/ter_dict ] 
+then 
+	echo "directory is present no need to creat";
+else
+	mkdir ~/ter_dict;
+	cd ~/ter_dict;
+	touch history.csv ;
+fi
